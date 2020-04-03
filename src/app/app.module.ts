74c8da14cb211bbceffaf413bbdbddef87268444
DGibbons1 <d.gibbons1@outlook.com>
2020-04-03 20:11:44 +0100

    Create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ShoppinglistComponent } from './shoppinglist/shoppinglist.component';
+import { RecipelistComponent } from './home/recipelist/recipelist.component';
+import { ReceipedetailComponent } from './receipedetail/receipedetail.component';
+import { RecepiedetailComponent } from './home/recepiedetail/recepiedetail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ShoppinglistComponent,
+    RecipelistComponent,
+    ReceipedetailComponent,
+    RecepiedetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/receipedetail/receipedetail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-receipedetail',
+  templateUrl: './receipedetail.component.html',
+  styleUrls: ['./receipedetail.component.css']
+})
+export class ReceipedetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
