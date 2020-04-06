af60980a53368e9ec1d35ae0eed3bb62d4b2090a
DGibbons1 <d.gibbons1@outlook.com>
2020-04-06 14:32:41 +0100

    Add routing function

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+// Import each Component required for routing
+import { HomeComponent } from './home/home.component';
+import { ShoppinglistComponent } from './shoppinglist/shoppinglist.component';
 
-const routes: Routes = [];
+// Declare Routes
+const routes: Routes = [
+  { path: 'recipes', component: HomeComponent },
+  { path: 'shoppinglist',  component: ShoppinglistComponent},
+  {
+    path: '',
+    redirectTo: '/recipes',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
